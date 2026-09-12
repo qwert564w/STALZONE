d883acf07a3fc1c2a88777af1f72c9444fbea871
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 23:25:27 +0300

    [GORILLA] feat: update triggerbot.h with full implementation

--- src/features/combat/triggerbot.h
+++ src/features/combat/triggerbot.h
@@ -1,11 +1,30 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-#include "../module.h"
+#include <cstdint>
+#include <chrono>
+
+namespace VV {
 namespace Combat {
-    class Triggerbot : public Module {
-    public:
-        Triggerbot() { name = "triggerbot"; }
-        void OnUpdate() override;
-        void OnRenderMenu() override;
-        void OnRenderOverlay() override;
-    };
-}
+
+class Triggerbot {
+public:
+    static bool s_enabled;
+    static int s_delayMin;
+    static int s_delayMax;
+    static float s_missChance;
+    static bool s_headOnly;
+    static bool s_visibilityCheck;
+    
+    static void OnUpdate();
+    static void OnRenderMenu();
+    static void OnRenderOverlay();
+    
+private:
+    static bool IsCrosshairOnEnemy();
+    static bool IsVisible(void* player);
+    static void PerformClick();
+    static std::chrono::steady_clock::time_point s_lastClick;
+};
+
+} // namespace Combat
+} // namespace VV
