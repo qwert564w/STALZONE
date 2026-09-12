bb8879b3fd84db844f623717c4abf77eccf401a7
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 22:19:15 +0300

    refactor: update menu.h with tabs for new features

--- src/gui/menu.h
+++ src/gui/menu.h
@@ -1,5 +1,18 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-namespace Menu {
-    void Init();
-    void Render();
+#include <string>
+
+namespace VV {
+    namespace GUI {
+        class Menu {
+        public:
+            static void Render();
+            static void Init();
+            static bool s_isOpen;
+        private:
+            static void RenderVisualTab();
+            static void RenderMiscTab();
+            static void RenderBypassTab();
+        };
+    }
 }
