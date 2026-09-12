35ca76649215f6df5bfd891bb1d100bfe86eab36
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 23:25:43 +0300

    [GORILLA] feat: update no_recoil.h with full implementation

--- src/features/combat/no_recoil.h
+++ src/features/combat/no_recoil.h
@@ -1,11 +1,26 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-#include "../module.h"
+#include <cstdint>
+
+namespace VV {
 namespace Combat {
-    class NoRecoil : public Module {
-    public:
-        NoRecoil() { name = "no_recoil"; }
-        void OnUpdate() override;
-        void OnRenderMenu() override;
-        void OnRenderOverlay() override;
-    };
-}
+
+class NoRecoil {
+public:
+    static bool s_enabled;
+    static float s_strength;
+    static bool s_verticalOnly;
+    static bool s_horizontalOnly;
+    
+    static void OnUpdate();
+    static void OnRenderMenu();
+    static void OnRenderOverlay();
+    
+private:
+    static void CompensateRecoil();
+    static float s_lastPunchAngleX;
+    static float s_lastPunchAngleY;
+};
+
+} // namespace Combat
+} // namespace VV
