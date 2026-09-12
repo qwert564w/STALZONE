cdbd80af356113ca54d835e213a071d2b2763428
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 23:27:27 +0300

    [GORILLA] feat: update chams.h with full implementation

--- src/features/visual/chams.h
+++ src/features/visual/chams.h
@@ -1,11 +1,22 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-#include "../module.h"
+
+namespace VV {
 namespace Visual {
-    class Chams : public Module {
-    public:
-        Chams() { name = "chams"; }
-        void OnUpdate() override;
-        void OnRenderMenu() override;
-        void OnRenderOverlay() override;
-    };
-}
+
+class Chams {
+public:
+    static bool s_enabled;
+    static bool s_visibleOnly;
+    static float s_colorEnemy[4];
+    static float s_colorAlly[4];
+    static float s_colorVisible[4];
+    
+    static void OnUpdate();
+    static void OnRenderMenu();
+    static void OnRenderOverlay();
+    static void ApplyChams(void* player, void* material);
+};
+
+} // namespace Visual
+} // namespace VV
