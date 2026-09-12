720db12b11a1ccc9649c3bfaa138d4f05409c378
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 23:27:49 +0300

    [GORILLA] feat: update corpse_esp.h with full implementation

--- src/features/visual/corpse_esp.h
+++ src/features/visual/corpse_esp.h
@@ -1,11 +1,21 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-#include "../module.h"
+
+namespace VV {
 namespace Visual {
-    class CorpseEsp : public Module {
-    public:
-        CorpseEsp() { name = "corpse_esp"; }
-        void OnUpdate() override;
-        void OnRenderMenu() override;
-        void OnRenderOverlay() override;
-    };
-}
+
+class CorpseESP {
+public:
+    static bool s_enabled;
+    static bool s_showLoot;
+    static bool s_showSkull;
+    static float s_color[4];
+    static float s_maxDistance;
+    
+    static void OnUpdate();
+    static void OnRenderMenu();
+    static void OnRenderOverlay();
+};
+
+} // namespace Visual
+} // namespace VV
