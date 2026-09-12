0092c12f960c288a9bbb63cc1dd253a099192b27
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 23:27:12 +0300

    [GORILLA] feat: update player_esp.h with full implementation

--- src/features/visual/player_esp.h
+++ src/features/visual/player_esp.h
@@ -1,11 +1,24 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-#include "../module.h"
+
+namespace VV {
 namespace Visual {
-    class PlayerEsp : public Module {
-    public:
-        PlayerEsp() { name = "player_esp"; }
-        void OnUpdate() override;
-        void OnRenderMenu() override;
-        void OnRenderOverlay() override;
-    };
-}
+
+class PlayerESP {
+public:
+    static bool s_enabled;
+    static bool s_showBoxes;
+    static bool s_showNames;
+    static bool s_showHealth;
+    static bool s_showDistance;
+    static bool s_showBones;
+    static float s_colorEnemy[4];
+    static float s_colorAlly[4];
+    
+    static void OnUpdate();
+    static void OnRenderMenu();
+    static void OnRenderOverlay();
+};
+
+} // namespace Visual
+} // namespace VV
