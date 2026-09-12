ee9fc6980e3e859856176f436174603e12e4e245
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-12 23:28:07 +0300

    [GORILLA] feat: update loot_esp.h with full implementation

--- src/features/visual/loot_esp.h
+++ src/features/visual/loot_esp.h
@@ -1,11 +1,25 @@
+// COPYRIGHT VV.EXE Development Team 2026. All rights reserved.
 #pragma once
-#include "../module.h"
+
+namespace VV {
 namespace Visual {
-    class LootEsp : public Module {
-    public:
-        LootEsp() { name = "loot_esp"; }
-        void OnUpdate() override;
-        void OnRenderMenu() override;
-        void OnRenderOverlay() override;
-    };
-}
+
+class LootESP {
+public:
+    static bool s_enabled;
+    static int s_minRarity;
+    static float s_maxDistance;
+    static bool s_showIcons;
+    static bool s_showNames;
+    static float s_colorCommon[4];
+    static float s_colorRare[4];
+    static float s_colorEpic[4];
+    static float s_colorLegendary[4];
+    
+    static void OnUpdate();
+    static void OnRenderMenu();
+    static void OnRenderOverlay();
+};
+
+} // namespace Visual
+} // namespace VV
